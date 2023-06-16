e8fc5661d9badda545367edcbd9903f9ef25ad69
BharathiGorli <104137325+BharathiGorli@users.noreply.github.com>
2023-06-16 04:52:51 -0700

    Add files via upload

---
+++ internpe task 3/NumberGuessingGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String play="yes";
+        while(play.equals("yes")){
+            Random rand= new Random();
+            int randNum =rand.nextInt(100);
+            int guess=-1;
+            int tries=0;
+            while(guess!=randNum){
+                System.out.print("Guess a number between 1 and 100:");
+                guess=sc.nextInt();
+                tries++;
+                if(guess==randNum){
+                    System.out.println("Correct! You Win!");
+                    System.out.println("It only took you "+tries+ " guesses!");
+                    System.out.println("Would you like to play again? Yes or No: ");
+                    play=sc.next().toLowerCase();
+                }
+                else if(guess>randNum){
+                    System.out.println("Nope! The number is higher, Guess again.");
+                }
+                else{
+                    System.out.println("NOpe! The number is lower, Guess again.");}
+            }}
+        sc.close();
+    }
+}
